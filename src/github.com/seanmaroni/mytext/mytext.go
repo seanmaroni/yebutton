d844863b244b6f1f74f913e90bc72ef941c9c135
Sean Newman Maroni <sean@betaversity.com>
2016-10-22 18:31:15 -0400

    second

---
+++ src/github.com/seanmaroni/mytext/mytext.go
@@ -0,0 +1,14 @@
+package mytext
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello. This is our first Go web app for Google App Engine")
+}
